4f8023b90601a1327185c64c54ea7307b7ca7dea
Vaderiana <120415111+Vaderiana@users.noreply.github.com>
2023-01-08 15:24:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int tiketPrice = 100;
+        int price = 20;
+        int bonus = tiketPrice / price;
+        System.out.println("Количество начисленных миль составит " + bonus);
+    }
+}
